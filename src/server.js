f739ccd9faa1ff89522c34eaf4dabd36113137b6
hai <tienhai2182001@gmail.com>
2023-06-23 21:59:43 +0700

    init7

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 // const express = require('express')
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
+require('dotenv').config();
+
 const app = express()
-const port = 3000
+const port = process.env.PORT||3000;// ||3000 backup
+console.log(`>>>check port:`,port)//kiểm tra cổng port
 
 // sendFile will go here
 configViewEngine(app);
