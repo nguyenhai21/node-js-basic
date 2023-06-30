8034f1ec32d88ff2e20ab01c5282b82be49ec6b8
hai <tienhai2182001@gmail.com>
2023-06-30 10:37:04 +0700

    init11

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,13 +1,20 @@
 // get the client
 // const mysql = require('mysql2');
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 
-// create the connection to database
-const connection = mysql.createConnection({
+console.log('create connection pool....');
+const pool=mysql.createPool({
   host: 'localhost',
   user: 'root',
   database: 'nodejsbasic'
-});
+})
+
+// create the connection to database
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'nodejsbasic'
+// });
 
 // // simple query
 // connection.query(
@@ -19,4 +26,4 @@ const connection = mysql.createConnection({
 //     console.log(results[0]); // results contains rows returned by server
 //   }
 // );
-export default connection;
+export default pool;
--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,27 +1,35 @@
-import connection from '../configs/connectDB';
+import pool from '../configs/connectDB';
 
-let getHomepage =(req,res)=>{
+let getHomepage =async(req,res)=>{
     let data=[];
-    connection.query(
-        'SELECT * FROM `users` ',
-        function(err, results, fields) {
-        // console.log(`>>> check mysql`)
-        // console.log(results); // results contains rows returned by server
-        // console.log(fields); // fields contains extra meta data about results, if available
-        results.map((row)=>{
-            data.push({
-                id: row.id,
-                email: row.email,
-                address: row.address,
-                firstName: row.firstName,
-                lastName:row.lastName
-            })
-        });   
-            // console.log(`>>>checkdata: `,typeof(data),JSON.stringify(data))
-            return res.render('index.ejs',{dataUser: data})   
-    });
-        
+    // connection.query(
+    //     'SELECT * FROM `users` ',
+    //     function(err, results, fields) {
+    //     // console.log(`>>> check mysql`)
+    //     // console.log(results); // results contains rows returned by server
+    //     // console.log(fields); // fields contains extra meta data about results, if available
+    //     results.map((row)=>{
+    //         data.push({
+    //             id: row.id,
+    //             email: row.email,
+    //             address: row.a ddress,
+    //             firstName: row.firstName,
+    //             lastName:row.lastName
+    //         })
+    //     });   
+    //         // console.log(`>>>checkdata: `,typeof(data),JSON.stringify(data))
+    //         // return res.render('index.ejs',{dataUser: data})   
+    // });
+        const [rows, fields] = await pool.execute('SELECT * FROM users');
+        return res.render('index.ejs',{dataUser: rows,test: 'hai'})
+        console.log(`>>>check rows: `,rows)
+}
+let getDetailPage =async(req,res)=>{
+    let userID =req.params.id;
+    let user = await pool.execute('SELECT * FROM users WHERE id=?',[userID])
+    console.log(`check req params: `,user)
+    return res.send(JSON.stringify(user[0]))
 }
 module.exports={
-    getHomepage
+    getHomepage,getDetailPage
 }
