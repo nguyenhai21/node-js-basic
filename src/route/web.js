e9e4a3656db82012846c17d60e83888d91af5b79
hai <tienhai2182001@gmail.com>
2023-07-26 10:36:55 +0700

    init16

--- src/route/web.js
+++ src/route/web.js
@@ -2,6 +2,7 @@ import express from "express";
 import homeController from '../controller/homeController';
 import multer from 'multer';
 import path from 'path';
+import { error } from "console";
 var appRoot = require('app-root-path');
 let router = express.Router();
 
@@ -27,6 +28,7 @@ const imageFilter = function (req, file, cb) {
 };
 
 let upload = multer({ storage: storage, fileFilter: imageFilter });
+let upload1= multer({ storage: storage, fileFilter: imageFilter }).array('multiple_images',3);
 
 const initWebRoute =(app)=>{
     router.get('/',homeController.getHomepage);
@@ -38,6 +40,20 @@ const initWebRoute =(app)=>{
 
     router.get('/upload',homeController.getUploadFilePage);
     router.post('/upload-profile-pic',upload.single('profile_pic'),homeController.handleUploadFile)
+    router.post('/upload-multiple-images',(req,res,next)=>{
+        upload1(req,res,(err)=>{
+            if (err instanceof multer.MulterError && err.code === 'LIMIT_UNEXPECTED_FILE') {
+                res.send('LIMIT_UNEXPECTED_FILE')
+                //handle multer file limit error here
+            }else if(err){
+                res.send(err)
+            }
+            else{
+                next();
+                //make sure to call next() if all was well
+            }
+        })
+    },homeController.handleUploadMultipleFiles)
 
     router.get('/about', (req, res) => {
           res.send('i am Ben')
