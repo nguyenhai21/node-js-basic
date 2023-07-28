3cc2e8e883ee68ac88e30c5b5432f9455e4191eb
hai <tienhai2182001@gmail.com>
2023-07-28 22:13:18 +0700

    init17

--- src/server.js
+++ src/server.js
@@ -6,10 +6,21 @@ import initWebRoute from './route/web';
 import initAPIRoute from './route/api';
 
 require('dotenv').config();
+var morgan = require('morgan')
+
 
 const app = express()
 const port = process.env.PORT||3000;// ||3000 backup
 console.log(`>>>check port:`,port)//kiểm tra cổng port
+
+app.use((req, res, next) => {
+  //check => return res.send()
+  console.log('>>> run into my middleware')
+  console.log(req.method)
+  next();
+})
+
+app.use(morgan('combined'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -22,6 +33,11 @@ initWebRoute(app);
 
 //init api route
 initAPIRoute(app);
+
+//handle 404 not found
+app.use((req, res) => {
+  return res.render('404.ejs')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
